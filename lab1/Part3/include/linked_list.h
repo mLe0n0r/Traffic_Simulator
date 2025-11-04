6506c009586200c7eee105f2dda7e5716c7bd1be
Maria Leonor Pinto Guedes <up202107691@edu.fe.up.pt>
2025-11-04 23:06:46 +0000

    Errors added

--- lab1/Part3/include/linked_list.h
+++ lab1/Part3/include/linked_list.h
@@ -16,7 +16,8 @@ typedef struct list {
 typedef struct queue_list {
     int purpose;
     double time;
-    double arrival_to_general;
+    double arrival_to_general;  // arrival time to general system
+    double wait_prediction;     // predicted waiting time
     struct queue_list* next;
 } queue_list;
 
@@ -25,7 +26,7 @@ list* __remove(list* pointer);
 list* __add(list * pointer, int n_type, int n_purpose, double n_time, double n_duration);
 void __print(list* pointer);
 
-queue_list* __add_queue(queue_list* pointer, int n_purpose, double n_time, double arrivals_G);
-queue_list* __remove_queue(queue_list * pointer, int *purpose, double * arrival_S, double *arrival_G);
+queue_list* __add_queue(queue_list* pointer, int n_purpose, double n_time, double n_arrival_G, double wait_prediction);
+queue_list* __remove_queue(queue_list * pointer, int *purpose, double * arrival_time, double * arrival_G, double *wait_prediction);
 
 #endif
